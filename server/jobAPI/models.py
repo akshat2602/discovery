c081ca229765e3df4458da13e1c46b76d34f3058
Akshat Sharma <akshatsharma2602@gmail.com>
2023-01-30 08:49:31 +0530

    Changed and added more models for application workflow

--- server/jobAPI/models.py
+++ server/jobAPI/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 import enum
 import uuid
 
-from common.enums import StatusChoice, ApplicationStepChoice
+from common.enums import StatusChoice
 
 # Create your models here.
 class RoleChoice(enum.enum):
@@ -12,6 +12,16 @@ class RoleChoice(enum.enum):
     ADMIN = 1
     HR = 2
     TECH = 3
+
+
+class ApplicationStepChoice(enum.enum):
+    """Enum for job posting steps."""
+
+    APPLIED = 1
+    ASSIGNMENT = 2
+    INTERVIEW = 3
+    OFFER = 4
+    HIRED = 5
 
 
 class User(AbstractUser):
@@ -25,14 +35,75 @@ class JobPosting(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     status = models.IntegerField(choices=[(tag, tag.value) for tag in StatusChoice])
-    created_by = models.ForeignKey(
+    fk_created_by = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="job_postings"
     )
-    steps = models.ArrayField(
-        models.IntegerField(choices=[(tag, tag.value) for tag in ApplicationStepChoice])
-    )
+    # steps = models.ArrayField(
+    #     models.IntegerField(choices=[(tag, tag.value) for tag in ApplicationStepChoice])
+    # )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+class JobPostingSteps(models.Model):
+    """Model for job posting steps."""
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    fk_job_posting = models.ForeignKey(
+        JobPosting, on_delete=models.CASCADE, related_name="steps"
+    )
+    step = models.IntegerField(
+        choices=[(tag, tag.value) for tag in ApplicationStepChoice]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.fk_job_posting.title + " - " + self.step
+
+
+class CandidateApplication(models.Model):
+    """
+    Model for maintaining data for candidates applying to a job posting.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    fk_job_posting = models.ForeignKey(
+        JobPosting, on_delete=models.CASCADE, related_name="candidates"
+    )
+    rejected = models.BooleanField(default=False)
+    resume = models.FileField(upload_to="resumes/")
+    applied_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name + " - " + self.fk_job_posting.title
+
+
+class CandidateStatus(models.Model):
+    """
+    Model for maintaining the status of each candidate for each job posting.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    fk_job_posting_candidate = models.ForeignKey(
+        CandidateApplication, on_delete=models.CASCADE, related_name="statuses"
+    )
+    fk_job_step = models.ForeignKey(JobPostingSteps, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return (
+            self.fk_job_posting_candidate.name
+            + " - "
+            + self.fk_job_posting_candidate.fk_job_posting.title
+            + " - "
+            + self.step
+        )
