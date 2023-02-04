ca1fceb136aed52b6764012cb4783190c5bb2674
Akshat Sharma <akshatsharma2602@gmail.com>
2023-02-04 13:33:40 +0530

    Added function for better upload naming scheme

--- server/common/utils.py
+++ server/common/utils.py
@@ -28,3 +28,13 @@ def error_message(serializer, status_code):
     return CustomErrorSerializer(
         {"status": status_code, "message": serializer.errors}
     ).data
+
+
+def resume_upload_path(instance, filename):
+    """Resume upload path"""
+    ext = os.path.splitext(filename)[1]
+    path = "resumes/job_{}/candidate_{}.{}".format(
+        instance.fk_job_posting.id, instance.id, ext
+    )
+
+    return path
