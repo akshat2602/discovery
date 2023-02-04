3562faf3e18c4ec0f693641d0209680d98d307d9
Akshat Sharma <akshatsharma2602@gmail.com>
2023-02-04 13:36:05 +0530

    Added CRUD for candidate application

--- server/jobAPI/urls.py
+++ server/jobAPI/urls.py
@@ -1,9 +1,12 @@
-from .views import JobPostingViewSet, JobPostingStepViewSet
+from .views import JobPostingViewSet, JobPostingStepViewSet, CandidateApplicationViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 
 router.register(r"posting", JobPostingViewSet, basename="jobposting")
 router.register(r"steps", JobPostingStepViewSet, basename="jobpostingsteps")
+router.register(
+    r"application", CandidateApplicationViewSet, basename="candidateapplication"
+)
 
 urlpatterns = router.urls
