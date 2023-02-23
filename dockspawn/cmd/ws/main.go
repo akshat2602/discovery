6a5d1486e1ab90607a4c865dcbb449e14548443d
Akshat Sharma <akshatsharma2602@gmail.com>
2023-02-23 19:21:40 +0530

    Added terminal websocket code with more modularity

--- dockspawn/cmd/ws/main.go
+++ dockspawn/cmd/ws/main.go
@@ -2,13 +2,9 @@ package ws
 
 import (
 	"net/http"
-
-	"github.com/akshat2602/discovery/dockspawn/pkg/helper"
-	"go.uber.org/zap/zapcore"
 )
 
 type WsServer struct {
-	LogI     func(msg string, fields ...zapcore.Field)
 	ServeMux http.ServeMux
 }
 
@@ -17,9 +13,5 @@ func (ws *WsServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func InitializeWSServer() *WsServer {
-	return &WsServer{
-		LogI: func(msg string, fields ...zapcore.Field) {
-			helper.Logger.Info(msg, fields...)
-		},
-	}
+	return &WsServer{}
 }
