09aacd3e6a07771f71b7ae8b8d056891aaf4e50d
Burhanuddin Merchant <bmerchant945@gmail.com>
2023-02-24 01:33:07 +0530

    added the basic logic for create file, change file, read file, create container

--- dockspawn/main.go
+++ dockspawn/main.go
@@ -17,6 +17,18 @@ func main() {
 	cs.ServeMux.HandleFunc("/terminal", func(w http.ResponseWriter, r *http.Request) {
 		terminal.ServeTerminal(w, r)
 	})
+	cs.ServeMux.HandleFunc("/file/change", func(w http.ResponseWriter, r *http.Request) {
+		files.handleFileChange(w, r)
+	})
+	cs.ServeMux.HandleFunc("/file/read", func(w http.ResponseWriter, r *http.Request) {
+		files.handleFileRead(w, r)
+	})
+	cs.ServeMux.HandleFunc("/file/create", func(w http.ResponseWriter, r *http.Request) {
+		files.handleFileCreate(w, r)
+	})
+	cs.ServeMux.HandleFunc("/container/create", func(w http.ResponseWriter, r *http.Request) {
+		files.handleContainerCreation(w, r)
+	})
 	s := &http.Server{
 		Addr:         "localhost:8080",
 		Handler:      cs,
