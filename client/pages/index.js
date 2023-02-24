24524792306d166acac67be7adf14ec13ab57cc4
Akshat Sharma <akshatsharma2602@gmail.com>
2023-02-24 23:19:26 +0530

    Fixed hydration issue

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import JobPostings from "./jobPostings";
+import JobPostings from "./jobpostings";
 
 export default function Home() {
   return (
@@ -10,9 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body>
-        <JobPostings />
-      </body>
+      <JobPostings />
     </>
   );
 }
