19ac33365dcb62809be8332560076dde8b78a997
Akshat Sharma <akshatsharma2602@gmail.com>
2023-02-24 23:19:26 +0530

    Fixed hydration issue

--- client/themes/index.js
+++ client/themes/index.js
@@ -1,47 +1,37 @@
 import {
-	extendTheme,
-	withDefaultColorScheme,
-	theme as baseTheme,
-} from '@chakra-ui/react';
-// import { Input } from './components/input.theme';
-import { mode } from '@chakra-ui/theme-tools';
-// import { Button } from './components/button.theme';
-// import { Container } from './components/container.theme';
-// import { Select } from './components/select.theme';
+  extendTheme,
+  withDefaultColorScheme,
+  theme as baseTheme,
+} from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 export const customTheme = extendTheme(
-	{
-		fonts: {
-			body: 'poppins',
-			heading: 'poppins',
-		},
-		colors: {
-			primary: {
-				50: '#00ADB5',
-			},
-			border: '#646D73',
-			dark: {
-				50: '#303841',
-			},
-			light: {
-				50: '#3A4750',
-			},
-		},
-		styles: {
-			global: (props) => ({
-				body: {
-					bg: mode('#303841', '#303841')(props),
-				},
-				'*::placeholder': {
-					color: mode('#3A4750', '#3A4750')(props),
-				},
-			}),
-		},
-		// components: {
-		// 	Input,
-		// 	Button,
-		// 	Container,
-		// 	Select,
-		// },
-	},
-	withDefaultColorScheme({ colorScheme: 'primary' }),
+  {
+    fonts: {
+      body: "poppins",
+      heading: "poppins",
+    },
+    colors: {
+      primary: {
+        50: "#00ADB5",
+      },
+      border: "#646D73",
+      dark: {
+        50: "#303841",
+      },
+      light: {
+        50: "#3A4750",
+      },
+    },
+    styles: {
+      global: (props) => ({
+        body: {
+          bg: mode("#303841", "#303841")(props),
+        },
+        "*::placeholder": {
+          color: mode("#3A4750", "#3A4750")(props),
+        },
+      }),
+    },
+  },
+  withDefaultColorScheme({ colorScheme: "primary" })
 );
