8090c536aa1acc3ad18a111ded76783e5b936387
Akshat Sharma <akshatsharma2602@gmail.com>
2023-03-01 20:52:35 +0530

    Added title to all pages

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,44 +1,50 @@
 import { Flex, Text, Input, Button, Center } from "@chakra-ui/react";
 import Link from "next/link";
+import Head from "next/head";
 
 const Login: React.FC = () => {
   return (
-    <Flex h="100vh" alignItems="center" justify="center">
-      <Flex
-        flexDirection="column"
-        bg={"light.50"}
-        p={12}
-        borderRadius={8}
-        boxShadow="lg"
-      >
-        <Center>
-          <Text mb={6} fontSize={"24"}>
-            Login
-          </Text>
-        </Center>
-        <Input
-          placeholder="johndoe@gmail.com"
-          type="email"
-          variant="filled"
-          bgColor={"dark.50"}
-          mb={3}
-        />
-        <Input
-          placeholder="**********"
-          type="password"
-          variant="filled"
-          bgColor={"dark.50"}
-          mb={6}
-        />
-        <Center>
-          <Link href={"/jobpostings"}>
-            <Button bgColor="primary.400" mb={8}>
-              Log In
-            </Button>
-          </Link>
-        </Center>
+    <>
+      <Head>
+        <title>Discovery | Login</title>
+      </Head>
+      <Flex h="100vh" alignItems="center" justify="center">
+        <Flex
+          flexDirection="column"
+          bg={"light.50"}
+          p={12}
+          borderRadius={8}
+          boxShadow="lg"
+        >
+          <Center>
+            <Text mb={6} fontSize={"24"}>
+              Login
+            </Text>
+          </Center>
+          <Input
+            placeholder="johndoe@gmail.com"
+            type="email"
+            variant="filled"
+            bgColor={"dark.50"}
+            mb={3}
+          />
+          <Input
+            placeholder="**********"
+            type="password"
+            variant="filled"
+            bgColor={"dark.50"}
+            mb={6}
+          />
+          <Center>
+            <Link href={"/dashboard/jobs"}>
+              <Button bgColor="primary.400" mb={8}>
+                Log In
+              </Button>
+            </Link>
+          </Center>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 };
 
