52b5959534f9bd7e3302d538f3f4e08b398a55fe
Akshat Sharma <akshatsharma2602@gmail.com>
2023-03-01 20:07:46 +0530

    Made more layouting changes

--- client/pages/dashboard/jobs/[id]/index.tsx
+++ client/pages/dashboard/jobs/[id]/index.tsx
@@ -23,6 +23,7 @@ import { useState } from "react";
 import Head from "next/head";
 
 import ListOfCandidates from "../../../../components/ListOfCandidates";
+import { DashboardLayout } from "../../../../components/Dashboard/DashboardLayout";
 
 const jobStepUnit = () => {
   return Object.assign(
@@ -48,172 +49,157 @@ const JobEdit: React.FC = () => {
       <Head>
         <title>Edit Job Posting</title>
       </Head>
-      <Flex direction={"column"} align="center" padding={4}>
-        <Flex w={"100%"} marginX={12} marginY={8} direction={"column"}>
-          <Flex>
-            <IconButton
-              aria-label="Back"
-              background="none"
-              _hover={{ background: "none" }}
-              icon={<BsArrowLeftShort size={24} />}
-              color="primary.400"
-              onClick={() => router.back()}
-            />
-            <Text fontSize={30} fontWeight={"bold"} noOfLines={1}>
-              Edit Job Details
-            </Text>
-          </Flex>
-          <Divider my={2} borderColor={"border"}></Divider>
-          <Container height={8} />
-          <FormControl>
-            <FormLabel fontSize={20}>Job Title</FormLabel>
-            <Input
-              type={"text"}
-              placeholder={"Software Development Engineer"}
-              text
-              size={"md"}
-              _placeholder={{ color: "gray.500" }}
-            />
-          </FormControl>
-          <Container height={8} />
-          <FormControl>
-            <FormLabel fontSize={20}>Job Description</FormLabel>
-            <Textarea
-              borderRadius={6}
-              placeholder={"This is a placeholder job description."}
-              size="md"
-              height={"auto"}
-              rows={10}
-              text
-              _placeholder={{ color: "gray.500" }}
-            />
-          </FormControl>
-          <Container height={8} />
-          <Flex direction={"row"} justify={"center"}>
-            <Menu>
-              <MenuButton
-                backgroundColor={"light.50"}
-                as={Button}
-                rightIcon={<FiChevronDown />}
-              >
-                Job Location
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Pune</MenuItem>
-                <MenuItem>Mumbai</MenuItem>
-                <MenuItem>Bangalore</MenuItem>
-                <MenuItem>San Francisco</MenuItem>
-                <MenuItem>New York</MenuItem>
-                <MenuItem>Frankfurt</MenuItem>
-                <MenuItem>Manchester</MenuItem>
-              </MenuList>
-            </Menu>
-          </Flex>
-          <Container height={8} />
-          <FormControl>
-            <FormLabel fontSize={20}>Interview Rounds</FormLabel>
-            {jobSteps.map((step, index) => {
-              return (
-                <Card
-                  key={index}
-                  rounded={12}
-                  borderTop={"2px"}
-                  borderTopColor="primary.400"
-                  bgColor={"light.50"}
-                  mb={2}
+      <DashboardLayout>
+        <Flex marginX={5} marginY={8} direction={"column"} align="center">
+          <Flex w={"100%"} direction={"column"}>
+            <Flex>
+              <Text fontSize={30} fontWeight={"bold"} noOfLines={1}>
+                Edit Job Details
+              </Text>
+            </Flex>
+            <Divider my={2} borderColor={"border"}></Divider>
+            <Container height={8} />
+            <FormControl>
+              <FormLabel fontSize={20}>Job Title</FormLabel>
+              <Input
+                type={"text"}
+                placeholder={"Software Development Engineer"}
+                size={"md"}
+                _placeholder={{ color: "gray.500" }}
+              />
+            </FormControl>
+            <Container height={8} />
+            <FormControl>
+              <FormLabel fontSize={20}>Job Description</FormLabel>
+              <Textarea
+                borderRadius={6}
+                placeholder={"This is a placeholder job description."}
+                size="md"
+                height={"auto"}
+                rows={10}
+                _placeholder={{ color: "gray.500" }}
+              />
+            </FormControl>
+            <Container height={8} />
+            <Flex direction={"row"} justify={"center"}>
+              <Menu>
+                <MenuButton as={Button} rightIcon={<FiChevronDown />}>
+                  Job Location
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>Pune</MenuItem>
+                  <MenuItem>Mumbai</MenuItem>
+                  <MenuItem>Bangalore</MenuItem>
+                  <MenuItem>San Francisco</MenuItem>
+                  <MenuItem>New York</MenuItem>
+                  <MenuItem>Frankfurt</MenuItem>
+                  <MenuItem>Manchester</MenuItem>
+                </MenuList>
+              </Menu>
+            </Flex>
+            <Container height={8} />
+            <FormControl>
+              <FormLabel fontSize={20}>Interview Rounds</FormLabel>
+              {jobSteps.map((step, index) => {
+                return (
+                  <Card
+                    key={index}
+                    rounded={12}
+                    borderTop={"2px"}
+                    borderTopColor="primary.400"
+                    bgColor={"light.400"}
+                    mb={2}
+                  >
+                    <Text
+                      paddingTop={4}
+                      paddingRight={8}
+                      paddingLeft={8}
+                      fontSize={16}
+                    >
+                      Round {index + 1}
+                    </Text>
+                    <Flex
+                      direction={"row"}
+                      px={8}
+                      py={4}
+                      justify={"space-around"}
+                      align={"center"}
+                    >
+                      <Input
+                        type={"text"}
+                        value={jobSteps[index].roundName}
+                        size={"md"}
+                        placeholder={"Enter round name"}
+                        _placeholder={{ color: "gray.500" }}
+                        onChange={(e) => {
+                          jobSteps[index].roundName = e.target.value;
+                          setJobSteps([...jobSteps]);
+                        }}
+                      />
+                      <Container width={8} />
+                      <Stack direction={"row"}>
+                        <Text>Active</Text>
+                        <Switch
+                          isChecked={jobSteps[index].isActive}
+                          colorScheme={"teal"}
+                          size={"md"}
+                          onChange={(e) => {
+                            jobSteps[index].isActive = e.target.checked;
+                            setJobSteps([...jobSteps]);
+                          }}
+                        />
+                      </Stack>
+                      <Container width={8} />
+                      <Stack direction={"row"}>
+                        <Text>Completed</Text>
+                        <Switch
+                          isChecked={jobSteps[index].isCompleted}
+                          colorScheme={"teal"}
+                          size={"md"}
+                          onChange={(e) => {
+                            jobSteps[index].isCompleted = e.target.checked;
+                            setJobSteps([...jobSteps]);
+                          }}
+                        />
+                      </Stack>
+                      <Container width={8} />
+                      <IconButton
+                        aria-label="Delete"
+                        icon={<BsFillTrashFill />}
+                        fontSize={30}
+                        onClick={() => {
+                          jobSteps.splice(index, 1);
+                          setJobSteps([...jobSteps]);
+                        }}
+                      />
+                    </Flex>
+                  </Card>
+                );
+              })}
+              <Container height={4} />
+              <Center>
+                <Button
+                  leftIcon={<FiPlus size={24} />}
+                  onClick={() => {
+                    setJobSteps([...jobSteps, jobStepUnit()]);
+                  }}
                 >
-                  <Text
-                    paddingTop={4}
-                    paddingRight={8}
-                    paddingLeft={8}
-                    fontSize={16}
-                  >
-                    Round {index + 1}
-                  </Text>
-                  <Flex
-                    direction={"row"}
-                    px={8}
-                    py={4}
-                    justify={"space-around"}
-                    align={"center"}
-                  >
-                    <Input
-                      type={"text"}
-                      value={jobSteps[index].roundName}
-                      text
-                      size={"md"}
-                      placeholder={"Enter round name"}
-                      _placeholder={{ color: "gray.500" }}
-                      onChange={(e) => {
-                        jobSteps[index].roundName = e.target.value;
-                        setJobSteps([...jobSteps]);
-                      }}
-                    />
-                    <Container width={8} />
-                    <Stack direction={"row"}>
-                      <Text>Active</Text>
-                      <Switch
-                        isChecked={jobSteps[index].isActive}
-                        colorScheme={"teal"}
-                        size={"md"}
-                        onChange={(e) => {
-                          jobSteps[index].isActive = e.target.checked;
-                          setJobSteps([...jobSteps]);
-                        }}
-                      />
-                    </Stack>
-                    <Container width={8} />
-                    <Stack direction={"row"}>
-                      <Text>Completed</Text>
-                      <Switch
-                        isChecked={jobSteps[index].isCompleted}
-                        colorScheme={"teal"}
-                        size={"md"}
-                        onChange={(e) => {
-                          jobSteps[index].isCompleted = e.target.checked;
-                          setJobSteps([...jobSteps]);
-                        }}
-                      />
-                    </Stack>
-                    <Container width={8} />
-                    <IconButton
-                      aria-label="Delete"
-                      icon={<BsFillTrashFill />}
-                      color={"red.500"}
-                      fontSize={30}
-                      onClick={() => {
-                        jobSteps.splice(index, 1);
-                        setJobSteps([...jobSteps]);
-                      }}
-                    />
-                  </Flex>
-                </Card>
-              );
-            })}
-            <Container height={4} />
+                  Add Round
+                </Button>
+              </Center>
+            </FormControl>
+
+            <ListOfCandidates />
+            <Container height={8} />
+
             <Center>
-              <Button
-                bgColor={"border"}
-                leftIcon={<FiPlus size={24} />}
-                onClick={() => {
-                  setJobSteps([...jobSteps, jobStepUnit()]);
-                }}
-              >
-                Add Round
+              <Button bgColor={"primary.400"} width="170px">
+                Save Edit
               </Button>
             </Center>
-          </FormControl>
-
-          <ListOfCandidates />
-          <Container height={8} />
-
-          <Center>
-            <Button bgColor={"primary.400"} width="170px">
-              Save Edit
-            </Button>
-          </Center>
+          </Flex>
         </Flex>
-      </Flex>
+      </DashboardLayout>
     </>
   );
 };
