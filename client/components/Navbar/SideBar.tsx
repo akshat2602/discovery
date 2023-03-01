622b8ee33f5d9fae5259f7ab4d8a5aa194eb21c0
Akshat Sharma <akshatsharma2602@gmail.com>
2023-03-01 18:29:47 +0530

    Added dashboard layout and better nested routing

--- client/components/Navbar/SideBar.tsx
+++ client/components/Navbar/SideBar.tsx
@@ -10,29 +10,25 @@ import {
   Icon,
   Link,
 } from "@chakra-ui/react";
-import profileIcon from "../public/profile.svg";
+import profileIcon from "../../public/profile.svg";
 import { FiMenu, FiBriefcase } from "react-icons/fi";
 import { useState } from "react";
-import Logo from "./Util/Logo";
-import Discovery from "./Util/Discovery";
+import Logo from "../Util/Logo";
+import Discovery from "../Util/Discovery";
 
-interface sideBarProps {
-  isOpen: boolean;
-  // setSideBarOpen is React useState Function
-  setSideBarOpen: Function;
-}
-
-const Sidebar: React.FC<sideBarProps> = ({ isOpen, setSideBarOpen }) => {
+const SideBar: React.FC = () => {
   const [selectedItem, setSelectedItem] = useState(1);
+  const [isOpen, setSideBarOpen] = useState(true);
+
   return (
     <Flex
       bg="light.50"
       pos="sticky"
-      left="5"
-      h="95vh"
-      marginTop="2.5vh"
+      // left="5"
+      h="100vh"
+      // marginTop="2.5vh"
       w={isOpen ? "220px" : "90px"}
-      rounded={24}
+      // rounded={24}
       boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
       flexDir="column"
       justifyContent="space-between"
@@ -60,23 +56,23 @@ const Sidebar: React.FC<sideBarProps> = ({ isOpen, setSideBarOpen }) => {
           <Logo isOpen={isOpen} />
           <Discovery isOpen={isOpen} />
         </Flex>
+
         <Flex mt={30} flexDir="column" w={isOpen ? "170px" : "40px"}>
           <Menu>
             <Link
-              backgroundColor={selectedItem == 1 ? "primary.50" : "none"}
+              backgroundColor={selectedItem == 1 ? "primary.400" : "none"}
               p={3}
               borderRadius={8}
               _hover={{ textDecor: "none", backgroundColor: "#AEC8CA" }}
               w={"100%"}
             >
               <Flex align={"center"} w="100%" height={"8px"} pt={2} pb={2}>
-                <Icon as={FiBriefcase} fontSize={"18px"} color={"white"} />
+                <Icon as={FiBriefcase} fontSize={"18px"} />
                 <Text
                   ml={5}
                   display={isOpen ? "flex" : "none"}
                   fontSize={18}
                   fontWeight={"medium"}
-                  textColor={"white"}
                 >
                   Jobs
                 </Text>
@@ -100,7 +96,7 @@ const Sidebar: React.FC<sideBarProps> = ({ isOpen, setSideBarOpen }) => {
           <Flex flexDir="column" ml={4} display={isOpen ? "flex" : "none"}>
             <Heading
               fontSize="18"
-              textColor={"white"}
+              color={"white"}
               fontWeight={"medium"}
               maxWidth="130px"
               textOverflow={"clip"}
@@ -110,7 +106,6 @@ const Sidebar: React.FC<sideBarProps> = ({ isOpen, setSideBarOpen }) => {
             </Heading>
             <Text
               fontSize="12"
-              textColor={"white"}
               fontWeight={"thin"}
               maxWidth="130px"
               textOverflow={"clip"}
@@ -125,4 +120,4 @@ const Sidebar: React.FC<sideBarProps> = ({ isOpen, setSideBarOpen }) => {
   );
 };
 
-export default Sidebar;
+export default SideBar;
