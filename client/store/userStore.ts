a73109096faca7dc0b5fe9ef572ee550286e3964
Akshat Sharma <akshatsharma2602@gmail.com>
2023-03-18 00:12:37 +0530

    Fixed error handling

--- client/store/userStore.ts
+++ client/store/userStore.ts
@@ -1,1 +1,20 @@
+import { StateCreator } from "zustand";
+
+export interface userStateInterface {
+  pk: number;
+  username: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+}
+
+export const createUserStore: StateCreator<userStateInterface> = (set) => ({
+  pk: 0,
+  username: "",
+  email: "",
+  first_name: "",
+  last_name: "",
+  setUserInfo: (userInfo: userStateInterface) => set(userInfo),
+});
+
 export {};
