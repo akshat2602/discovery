c1cfcbbf06cdd1e939608b14dda6b09701d22734
Burhanuddin Merchant <bmerchant945@gmail.com>
2023-03-26 17:49:45 +0530

    migrated the candidate assignment models

--- server/migrations.sh
+++ server/migrations.sh
@@ -1,4 +1,4 @@
-python manage.py makemigrations jobAPI
+python manage.py makemigrations jobAPI assignmentAPI
 python manage.py makemigrations
 python manage.py migrate
 python manage.py shell < tools/create_superuser.py
