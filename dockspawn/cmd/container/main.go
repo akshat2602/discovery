abd90c2f9fb6f21100928d234ac70bd28732bbbe
Burhanuddin Merchant <bmerchant945@gmail.com>
2023-03-26 13:50:40 +0530

    added container removal logic

--- dockspawn/cmd/container/main.go
+++ dockspawn/cmd/container/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/akshat2602/discovery/dockspawn/pkg/helper"
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 )
@@ -17,13 +18,16 @@ import (
 type ContainerStopRequestBody struct {
 	ContainerID string `json:"container_id"`
 }
+type ContainerRemoveRequestBody struct {
+	ContainerID string `json:"container_id"`
+}
 
 func HandleContainerStop(w http.ResponseWriter, r *http.Request) {
 	var csrb = ContainerStopRequestBody{}
 	err := json.NewDecoder(r.Body).Decode(&csrb)
 	if err != nil {
 		helper.Logger.Sugar().Info("Error ", err)
-		// respond with error
+		// TODO: respond with error
 		return
 	}
 
@@ -38,8 +42,37 @@ func HandleContainerStop(w http.ResponseWriter, r *http.Request) {
 	err = cli.ContainerStop(ctx, container_id, container.StopOptions{})
 	if err != nil {
 		helper.Logger.Sugar().Info("Error ", err)
-		// respond with error
+		// TODO: respond with error
 		return
 	}
 	w.Write([]byte("Successfully Stopped the container with id " + container_id))
+}
+
+func HandleContainerRemove(w http.ResponseWriter, r *http.Request) {
+	var crmrb = ContainerStopRequestBody{}
+	err := json.NewDecoder(r.Body).Decode(&crmrb)
+	if err != nil {
+		helper.Logger.Sugar().Info("Error ", err)
+		// TODO: respond with error
+		return
+	}
+
+	container_id := crmrb.ContainerID
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		helper.Logger.Sugar().Info("Error ", err)
+	}
+	defer cli.Close()
+	cli.ContainerRemove(ctx, container_id, types.ContainerRemoveOptions{})
+	err = cli.ContainerStop(ctx, container_id, container.StopOptions{})
+
+	// TODO: File Removal logic
+	if err != nil {
+		helper.Logger.Sugar().Info("Error ", err)
+		// TODO: respond with error
+		return
+	}
+
+	w.Write([]byte("Successfully Removed the container with id " + container_id))
 }
