9205859e2a44431caa02023ad1841b5653af0acd
Burhanuddin Merchant <bmerchant945@gmail.com>
2023-03-26 17:24:47 +0530

    updated assignmentAPI models and added model admins (not migrated or tested yet)

--- server/assignmentAPI/admin.py
+++ server/assignmentAPI/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import CandidateAssignment, CandidateAssignmentResult, AssignmentType
 
 # Register your models here.
+
+
+class CandidateAssignmentAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "fk_job_posting_step",
+        "fk_assignment_type",
+        "total_test_cases",
+        "cutoff_test_cases",
+        "duration",
+        "expired",
+        "active_from",
+        "active_upto",
+        "created_at",
+        "updated_at",
+    ]
+    list_filter = ["fk_assignment_type"]
+    sortable_by = ["active_from", "active_upto"]
+
+
+class CandidateAssignmentResultAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "fk_candidate_application",
+        "fk_candidate_assignment",
+        "container_id",
+        "test_cases_passed",
+        "attempted",
+        "start_time",
+        "end_time",
+        "created_at",
+        "updated_at",
+    ]
+    list_filter = ["attempted", "fk_candidate_assignment"]
+
+
+class AssignmentTypeAdmin(admin.ModelAdmin):
+    list_display = ["name", "docker_file", "created_at", "updated_at"]
+
+
+admin.site.register(CandidateAssignment, CandidateAssignmentAdmin)
+admin.site.register(CandidateAssignmentResult, CandidateAssignmentResultAdmin)
+admin.site.register(AssignmentType, AssignmentTypeAdmin)
