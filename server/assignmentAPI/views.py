a4e0223a08619c3a1f2e06de6f6082217285b5fe
Burhanuddin Merchant <bmerchant945@gmail.com>
2023-03-28 23:54:04 +0530

    added CRUD routes for candidate assignment

--- server/assignmentAPI/views.py
+++ server/assignmentAPI/views.py
@@ -1,3 +1,128 @@
-# from django.shortcuts import render
+from rest_framework import viewsets, status as http_status
+from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
+from django.core.exceptions import ValidationError
+from common.utils import CustomErrorSerializer, error_message
+from .serializers import (
+    CandidateAssignmentFetchSerializer,
+    CandidateAssignmentCreateSerializer,
+)
+from .models import CandidateAssignment
 
-# Create your views here.
+
+# TODO: Add authentication and permission management
+
+
+class CandidateAssignmentViewSet(viewsets.ViewSet):
+    """Viewset for Candidate Assignment"""
+
+    @swagger_auto_schema(
+        operation_description="Create a new Candidate Assignment",
+        request_body=CandidateAssignmentCreateSerializer,
+        responses={
+            http_status.HTTP_201_CREATED: CandidateAssignmentCreateSerializer,
+            http_status.HTTP_400_BAD_REQUEST: CustomErrorSerializer,
+        },
+    )
+    def create(self, request):
+        """
+        Create a new Candidate Assignment for a Job Step
+        """
+
+        serialized = CandidateAssignmentCreateSerializer(request.data, many=True)
+        if serialized.is_valid():
+            serialized.save()
+            return Response(data=serialized.data, status=http_status.HTTP_201_CREATED)
+        return Response(
+            error_message(serialized, http_status.HTTP_400_BAD_REQUEST),
+            status=http_status.HTTP_400_BAD_REQUEST,
+        )
+
+    @swagger_auto_schema(
+        operation_description="Fetch all candidate assignment",
+        responses={
+            http_status.HTTP_200_OK: CandidateAssignmentFetchSerializer(many=True),
+        },
+    )
+    def list(self, request):
+        """
+        Fetch all Candidate Assignments
+        """
+
+        candidate_assignments = CandidateAssignment.objects.all()
+        serialized = CandidateAssignmentFetchSerializer(
+            candidate_assignments, many=True
+        )
+        return Response(serialized.data, status=http_status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_description="Update a Candidate Assignment",
+        responses={
+            http_status.HTTP_200_OK: CandidateAssignmentCreateSerializer,
+            http_status.HTTP_404_NOT_FOUND: CustomErrorSerializer,
+            http_status.HTTP_400_BAD_REQUEST: CustomErrorSerializer,
+        },
+    )
+    def update(self, request, pk=None):
+        """
+        Update a Candidate Assignment
+        """
+        candidate_assignment_id = pk
+        try:
+            query = CandidateAssignment.objects.get(pk=candidate_assignment_id)
+        except CandidateAssignment.DoesNotExist:
+            return Response(
+                error_message(
+                    "Candidate Assignment not found", http_status.HTTP_404_NOT_FOUND
+                ),
+                status=http_status.HTTP_404_NOT_FOUND,
+            )
+        except ValidationError:
+            return Response(
+                error_message("Invalid ID", http_status.HTTP_400_BAD_REQUEST),
+                status=http_status.HTTP_400_BAD_REQUEST,
+            )
+
+        serialized = CandidateAssignmentCreateSerializer(
+            query, data=request.data, partial=True
+        )
+        if serialized.is_valid():
+            serialized.save()
+            return Response(data=serialized.data, status=http_status.HTTP_200_OK)
+        return Response(
+            error_message(serialized, http_status.HTTP_400_BAD_REQUEST),
+            status=http_status.HTTP_400_BAD_REQUEST,
+        )
+
+    @swagger_auto_schema(
+        operation_description="Delete a Candidate Assignment",
+        responses={
+            http_status.HTTP_204_NO_CONTENT: "",
+            http_status.HTTP_404_NOT_FOUND: CustomErrorSerializer,
+            http_status.HTTP_400_BAD_REQUEST: CustomErrorSerializer,
+        },
+    )
+    def destroy(self, request, pk=None):
+        """
+        Delete a Candidate Assignment
+        """
+
+        candidate_assignment_id = pk
+
+        try:
+            query = CandidateAssignment.objects.get(pk=candidate_assignment_id)
+        except CandidateAssignment.DoesNotExist:
+            return Response(
+                error_message(
+                    "Candidate Assignment not found", http_status.HTTP_404_NOT_FOUND
+                ),
+                status=http_status.HTTP_404_NOT_FOUND,
+            )
+
+        except ValidationError:
+            return Response(
+                error_message("Invalid ID", http_status.HTTP_400_BAD_REQUEST),
+                status=http_status.HTTP_400_BAD_REQUEST,
+            )
+        query.delete()
+        return Response(status=http_status.HTTP_204_NO_CONTENT)
