d77b6c1c318ba7e40f05035ba490da287f165d51
Burhanuddin Merchant <bmerchant945@gmail.com>
2023-03-29 20:42:36 +0530

    added serializers, viewset and route for Candidate Assignment Result

--- server/assignmentAPI/urls.py
+++ server/assignmentAPI/urls.py
@@ -1,8 +1,15 @@
-from .views import CandidateAssignmentViewSet, AssignmentTypeViewSet
+from .views import (
+    CandidateAssignmentViewSet,
+    AssignmentTypeViewSet,
+    CandidateAssignmentResultViewSet,
+)
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r"type", AssignmentTypeViewSet, basename="assignmenttype")
+router.register(
+    r"result", CandidateAssignmentResultViewSet, basename="candidateassignmentresult"
+)
 router.register(r"", CandidateAssignmentViewSet, basename="candidateassignment")
 
 urlpatterns = router.urls
