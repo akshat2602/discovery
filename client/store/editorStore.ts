a020cace55856497569168521db2bfd7985b9fe9
Akshat Sharma <akshatsharma2602@gmail.com>
2023-04-10 18:45:06 +0530

    Added stores for folder structure working

--- client/store/editorStore.ts
+++ client/store/editorStore.ts
@@ -2,10 +2,13 @@ import { StateCreator } from "zustand";
 
 export interface editorFileInterface {
   activeTab: { path: string; value: string } | null;
+  availableTabs: { [key: string]: boolean };
+  // folderStructure: {}
 }
 
 export interface editorFileActionsInterface {
   setActiveTab: (path: string, value: string) => void;
+  addOrUpdateTab: (path: string) => void;
 }
 
 export const createEditorFileStore: StateCreator<
@@ -15,6 +18,7 @@ export const createEditorFileStore: StateCreator<
   editorFileInterface
 > = () => ({
   activeTab: null,
+  availableTabs: {},
 });
 
 export const setEditorFileStore: StateCreator<
@@ -22,10 +26,18 @@ export const setEditorFileStore: StateCreator<
   [],
   [],
   editorFileActionsInterface
-> = (set) => ({
+> = (set, get) => ({
   setActiveTab: (path, value) => {
-    set((state) => ({
+    set({
       activeTab: { path: path, value: value },
-    }));
+    });
+  },
+  addOrUpdateTab: (path) => {
+    const availableTabs = get().availableTabs;
+    Object.keys(availableTabs).forEach((key) => {
+      availableTabs[key] = false;
+    });
+    const newState = { ...availableTabs, [path]: true };
+    set({ availableTabs: newState });
   },
 });
