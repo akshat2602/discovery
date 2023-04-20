42b5c6a323ca3ed3f89dfbb26b9c62c6120eb43c
Akshat Sharma <akshatsharma2602@gmail.com>
2023-04-20 16:29:23 +0530

    Added resizable component and editor tabs

---
+++ client/components/Editor/EditorTabsComponent.tsx
@@ -0,0 +1,30 @@
+import { Box } from "@chakra-ui/react";
+
+import { EditorButtonComponent } from "./EditorButton";
+
+import { useBearStore } from "../../store/bearStore";
+
+export const EditorTabsComponent = () => {
+  const availableTabs = useBearStore((state) => state.availableTabs);
+
+  return (
+    <Box
+      display={"flex"}
+      pt={"5px"}
+      pb={"5px"}
+      h={"3vh"}
+      borderBottom={"1px solid #1f1f1f"}
+    >
+      {Object.keys(availableTabs).length > 0 &&
+        Object.entries(availableTabs).map((entries) => {
+          return (
+            <EditorButtonComponent
+              path={entries[0]}
+              isActive={entries[1]}
+              key={entries[0]}
+            />
+          );
+        })}
+    </Box>
+  );
+};
