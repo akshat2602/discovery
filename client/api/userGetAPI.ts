5adcdf4bd4b7fb0218160b2379ca1476ea9d0a3b
Akshat Sharma <akshatsharma2602@gmail.com>
2023-04-21 05:08:28 +0530

    Added job generation with per round candidates

--- client/api/userGetAPI.ts
+++ client/api/userGetAPI.ts
@@ -5,7 +5,7 @@ import { randomUserInterface } from "../types/randomUserType";
 
 export const userGetAPI = async (): Promise<randomUserInterface> => {
   const response = await axios.get(
-    "https://randomuser.me/api/?nat=in&results=100"
+    "https://randomuser.me/api/?nat=in&results=100&seed=abc"
   );
   return response.data;
 };
