8c2e97fa54f569d1dd8b092885e426894d2edc35
Akshat Sharma <akshatsharma2602@gmail.com>
2023-04-21 05:33:18 +0530

    candidate pipeline dynamic and candidate profile page

--- client/components/CandidatesPipeline.tsx
+++ client/components/CandidatesPipeline.tsx
@@ -15,16 +15,7 @@ interface CandidatesPipelineProps {
   job: jobInterface;
 }
 // Define types for the hiring stages and candidates data
-type Candidate = {
-  name: string;
-  rating: number;
-  daysAgo: number;
-};
 
-type HiringStage = {
-  stage: string;
-  candidates: Candidate[];
-};
 const colorList: string[] = [
   "#7ED376",
   "#7D9BD3",
@@ -33,72 +24,28 @@ const colorList: string[] = [
   "#C0A1C2",
   "#00ADB5",
 ];
-const hiringStages: HiringStage[] = [
-  {
-    stage: "APPLIED",
-    candidates: [
-      { name: "Deandre Barry", rating: 0, daysAgo: 10 },
-      { name: "Hubert Frederick", rating: 0, daysAgo: 12 },
-      { name: "Anton Green", rating: 0, daysAgo: 16 },
-      { name: "Jewell Spence", rating: 0, daysAgo: 15 },
-      { name: "Erin Gould", rating: 0, daysAgo: 13 },
-      { name: "Giovanni Wang", rating: 0, daysAgo: 18 },
-      { name: "Brock Heath", rating: 0, daysAgo: 19 },
-    ],
-  },
-  {
-    stage: "SCREENING",
-    candidates: [
-      { name: "Bernardo Carney", rating: 3, daysAgo: 7 },
-      { name: "Zack Beier", rating: 4, daysAgo: 6 },
-      { name: "Gregorio Senger", rating: 5, daysAgo: 8 },
-      { name: "Sandrine Ruecker", rating: 1, daysAgo: 9 },
-      { name: "Shawn Moss", rating: 4, daysAgo: 4 },
-      { name: "Zander Mante", rating: 3, daysAgo: 3 },
-      { name: "Aniya Huel", rating: 2, daysAgo: 7 },
-    ],
-  },
-  {
-    stage: "CODING TEST",
-    candidates: [
-      { name: "Deandre Barry", rating: 4, daysAgo: 10 },
-      { name: "Fidel Gutkowski", rating: 3, daysAgo: 6 },
-      { name: "Garett", rating: 4, daysAgo: 5 },
-      { name: "Richie Lubowitz", rating: 0, daysAgo: 7 },
-      { name: "Jocelyn Okon", rating: 1, daysAgo: 6 },
-    ],
-  },
-  {
-    stage: "INTERVIEW",
-    candidates: [
-      { name: "Olga Streich", rating: 4, daysAgo: 7 },
-      { name: "Demarcus Dooley", rating: 3, daysAgo: 8 },
-      { name: "Scott Lang", rating: 5, daysAgo: 9 },
-    ],
-  },
-  {
-    stage: "HR INTERVIEW",
-    candidates: [
-      { name: "Marielle James", rating: 2, daysAgo: 9 },
-      { name: "Kristina Effert", rating: 4, daysAgo: 2 },
-    ],
-  },
-  {
-    stage: "HIRED",
-    candidates: [{ name: "Easton Whener", rating: 4, daysAgo: 1 }],
-  },
-];
+
 type CandidateCardProps = {
   name: {
     title: string;
     first: string;
     last: string;
   };
-  // rating: number;
+  jobId: string;
   id: string;
-  dob: Date;
+  dob: {
+    date: string;
+    age: number;
+  };
 };
-const CandidateCard: React.FC<CandidateCardProps> = ({ name, id, dob }) => {
+
+const CandidateCard: React.FC<CandidateCardProps> = ({
+  name,
+  id,
+  dob,
+  jobId,
+}) => {
+  console.log(dob);
   const fg = useColorModeValue("light.200", "dark.200");
   const rating = Math.random() * 5;
   const router = useRouter();
@@ -142,9 +89,9 @@ const CandidateCard: React.FC<CandidateCardProps> = ({ name, id, dob }) => {
       textAlign="center"
       bgColor={fg}
       onClick={() => {
-        // router.push(`dashboard/job//candidate/${id}`);
-        console.log("here");
+        router.push(`/dashboard/job/${jobId}/candidate/${id}`);
       }}
+      cursor="pointer"
     >
       <Text fontSize="16" fontWeight="semibold" mb={2}>
         {name.title} {name.first} {name.last}
@@ -158,7 +105,7 @@ const CandidateCard: React.FC<CandidateCardProps> = ({ name, id, dob }) => {
           </Text>
         </Flex>
         <Text color="gray.500" fontSize="10" fontWeight="regular">
-          {/* {dob} */}DOB goes here
+          {new Date(dob.date).toDateString()}
         </Text>
       </Flex>
     </Box>
@@ -187,8 +134,9 @@ const HiringStagesList: React.FC<CandidatesPipelineProps> = (jobObject) => {
 
           {candidates?.map(({ id, name, dob }) => (
             <CandidateCard
-              key={id}
-              id={id}
+              jobId={jobObject.job.id}
+              key={id.value}
+              id={id.value}
               name={name}
               // rating={rating}
               dob={dob}
--- client/pages/dashboard/job/[jobId]/candidate/[candidateId]/index.tsx
+++ client/pages/dashboard/job/[jobId]/candidate/[candidateId]/index.tsx
@@ -1,12 +1,16 @@
-import { Flex, Text, Divider } from "@chakra-ui/react";
-import { Container } from "@chakra-ui/react";
+import { Flex, Text, Divider, Box, Button } from "@chakra-ui/react";
 
 import Head from "next/head";
+import { useRouter } from "next/router";
 
-import ListOfCandidates from "../../../../../../components/ListOfCandidates";
 import { DashboardLayout } from "../../../../../../components/Dashboard/DashboardLayout";
 
+import { useBearStore } from "../../../../../../store/bearStore";
+
 const JobEdit: React.FC = () => {
+  const router = useRouter();
+  const users = useBearStore((state) => state.users);
+  const user = users.find((user) => user.id.value === router.query.candidateId);
   return (
     <>
       <Head>
@@ -16,12 +20,20 @@ const JobEdit: React.FC = () => {
         <Flex marginX={5} marginY={8} direction={"column"} align="center">
           <Flex>
             <Text fontSize={30} fontWeight={"bold"} noOfLines={1}>
-              View Candidates
+              View Candidate
             </Text>
           </Flex>
           <Divider my={2} borderColor={"border"}></Divider>
-          <ListOfCandidates />
-          <Container height={8} />
+          <Box>
+            Please add the candidate's details here. This is a work in progress.
+          </Box>
+          <Button
+            onClick={() => {
+              router.push(`/assessment/7cc8f159-cb87-4c76-b282-cc3749622fcc`);
+            }}
+          >
+            View candidate's assignment
+          </Button>
         </Flex>
       </DashboardLayout>
     </>
